cca0acc5cf76b4ca9cebc3bc31525e3f69c2c6f2
Natheirx <Mustafanatheir1@gmail.com>
2021-02-28 03:28:52 -0500

    working image to pixels

--- start.py
+++ start.py
@@ -1,45 +1,41 @@
 import cv2 as cv
 import numpy as np
-FILENAME = "map.jpg"
-SIZE = 14
+import random
+FILENAME = "map2.jpg"
+WIDTH = 18
+HEIGHT = 24
 
 img = cv.imread(FILENAME, cv.IMREAD_GRAYSCALE)
+lightO = cv.imread("lightO.jpg", cv.IMREAD_GRAYSCALE)
+light1 = cv.imread("light1.jpg", cv.IMREAD_GRAYSCALE)
+darkO = cv.imread("darkO.jpg", cv.IMREAD_GRAYSCALE)
+dark1 = cv.imread("dark1.jpg", cv.IMREAD_GRAYSCALE)
 output = np.zeros((len(img), len(img[0])))
 
-font = cv.FONT_HERSHEY_SIMPLEX
 
-
-# np.random.randint()
-
-
-
-
-lst = []
-for i in range(int(round(len(img) / SIZE))):
-    for j in range(int(round((len(img[0]) / SIZE)))):
-        kernel = img[i * SIZE: (i + 1) * SIZE, j * SIZE: (j + 1)  * SIZE]
-        if np.mean(kernel) > np.mean(img):
-            cv.putText(output,str(np.random.randint(0, 2)),(j * SIZE, i * SIZE), font, 0.5, (255), 1, cv.LINE_AA)
-        # else:
-        #     cv.putText(output,'1',(j * SIZE, i * SIZE), font, 0.5, (0), 1, cv.LINE_AA)
-
+def setImage(img, x, y):
+    letter = cv.imread(img, cv.IMREAD_GRAYSCALE)
+    for i in range(len(letter)):
+        for j in range(len(letter[0])):
+            if x + i < len(output) and y + j < len(output[0]):
+                output[x + i, y + j] = letter[i, j] / 255
     
 
 
+# lst = []
+for i in range(int(round(len(img) / WIDTH))):
+    for j in range(int(round((len(img[0]) / HEIGHT)))):
+        kernel = img[i * WIDTH: (i + 1) * WIDTH, j * HEIGHT: (j + 1) * HEIGHT]
+        if np.mean(kernel) > np.mean(img) + 5:
+            setImage("dark1.jpg", i * WIDTH, j * HEIGHT)
+        else:
+            setImage("light1.jpg", i * WIDTH, j * HEIGHT)
+        # setImage("light1.jpg", i * HEIGHT, j * HEIGHT)
 
 
-# kernel = np.ones((28, 28)) / (28 **2) 
-# print(kernel)
-
-
-# print(output)
-
-# new = cv.filter2D(img, cv.IMREAD_GRAYSCALE, kernel)
-# print(new)
-# print(len(new))
 cv.imshow("image", output)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
 
-# cv2.imwrite("endproduct.png", img)
+# cv2.imwrite("endproduct.png", output)
